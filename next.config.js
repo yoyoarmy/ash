e840996f0102d00bc2db0a23a3709a77363be6d0
yoyoarmy <josepharmancollege@gmail.com>
2025-01-30 14:39:00 -0500

    Initial

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // Remove serverActions as it's now enabled by default
-  }
-}
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
